7f184180f94371215c47f30790977c298e639ba1
bhamal7 <78674911+bhamal7@users.noreply.github.com>
2021-06-02 22:25:47 -0500

    new file added

---
+++ 7.py
@@ -0,0 +1,6 @@
+#Upper CamelCase
+Bishal = 2
+#LOwer CamelCase
+bishal = 3
+#SnakeCase
+bis_hal = 4
---
+++ 8.py
@@ -0,0 +1,6 @@
+# Yes, variables in Python can be reassigned to a new value that is a different data type from its current value. 
+# In fact, variables can be reassigned to any valid value in Python, regardless of its current value.
+
+# Variables are essentially like an empty box, that can contain something like a string, number, or other value.
+#  When you assign it a value, the box will contain that value, and when you reassign it, it will empty out the old value, 
+#  and the new value will be placed inside of it.
